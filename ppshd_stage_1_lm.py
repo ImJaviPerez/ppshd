080c8ed462bfb9f1f55301e87666dab097e6fd1d
ImJaviPerez <ImJaviPerez@gmail.com>
2023-06-28 17:49:40 +0200

    Change number of patients with number of INITIAL patients

--- ppshd_stage_1_lm.py
+++ ppshd_stage_1_lm.py
@@ -15,20 +15,20 @@ import ppshd_cfg
 
 import pyomo.environ as pyo
 
-model = pyo.AbstractModel(name="Personnel and Patient Scheduling")
+model = pyo.AbstractModel(name="Personnel and Patient Scheduling. Stage I")
 
 
 # Parameter
-# N : Number of patients
-model.N = pyo.Param(within=pyo.PositiveIntegers)
+# M : Number of initial patients
+model.M = pyo.Param(within=pyo.PositiveIntegers)
 
-def N_rule(model):
-    return model.N >= 1
+def M_rule(model):
+    return model.M >= 1
 
-model.check_N = pyo.BuildCheck(rule=N_rule)
+model.check_M = pyo.BuildCheck(rule=M_rule)
 
-# N_set : set of patiens
-model.N_set = pyo.RangeSet(model.N)
+# M_set : set of patiens
+model.M_set = pyo.RangeSet(model.M)
 
 # Parameter
 # S: Number of physiotherapists
@@ -41,7 +41,7 @@ model.check_S = pyo.BuildCheck(rule=S_rule)
 
 # Parameter
 # t[i] : Treatment time of i-th patient in minutes
-model.t = pyo.Param(model.N_set, within=pyo.NonNegativeIntegers, initialize=0)
+model.t = pyo.Param(model.M_set, within=pyo.NonNegativeIntegers, initialize=0)
 
 
 # Parameter
@@ -56,18 +56,18 @@ model.check_T = pyo.BuildCheck(rule=T_rule)
 
 # Parameter
 # p[i] : Weight of priority level for i-th patient
-model.p = pyo.Param(model.N_set, within=pyo.PositiveIntegers)
+model.p = pyo.Param(model.M_set, within=pyo.PositiveIntegers)
 def p_rule(model, i):
     return model.p[i] >= 1
 
-model.check_p = pyo.BuildCheck(model.N_set, rule=p_rule)
+model.check_p = pyo.BuildCheck(model.M_set, rule=p_rule)
 
 
 # Decision variable
 # x[i] : 
 #   1 if i-th patient is selected
 #   0 otherwise
-model.x = pyo.Var(model.N_set, within=pyo.Binary, initialize=0)
+model.x = pyo.Var(model.M_set, within=pyo.Binary, initialize=0)
 
 
 # OBJETIVE FUNCTION AND CONSTRICTIONS ---------------
@@ -79,7 +79,7 @@ model.OBJ= pyo.Objective(rule=obj_rule, sense=pyo.maximize)
 
 # Constraint 1: SUM x * t <= T
 def enough_time_rule(model):
-    return pyo.sum_product(model.x, model.t, index=model.N_set) <= model.T
+    return pyo.sum_product(model.x, model.t, index=model.M_set) <= model.T
 
 model.enough_time = pyo.Constraint(rule=enough_time_rule)
 
@@ -125,8 +125,8 @@ def print_cosas():
     print(instance.name)
     # instance.T.pprint()
     # instance.N.pprint()
-    # instance.N_set.pprint()
-    for i in instance.N_set:
+    # instance.M_set.pprint()
+    for i in instance.M_set:
         if pyo.value(instance.x[i]) != 0:
             total_time += pyo.value(instance.x[i]) * pyo.value(instance.t[i])
             # print("x[", i, "]=", pyo.value(instance.x[i]), ",, t[", i, "]=", pyo.value(instance.t[i]), ",, total_time =", total_time)
@@ -146,7 +146,7 @@ def print_cosas():
     print("total_patients paper =", len(patients_paper_indices))
     print("total_time_paper =", total_time_paper)
     paper_obj = 0
-    for i in instance.N_set:
+    for i in instance.M_set:
         paper_obj += pyo.value(instance.p[i]) * pyo.value(instance.x[i])
     print("PAPER.OBJ =", paper_obj)
 print_cosas()
